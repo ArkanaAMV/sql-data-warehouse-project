07efec62e56791bce6f79189c656f53951de12f4
Angel Mellado <arkanaamv@gmail.com>
2026-09-19 18:37:20 -0300

    mi primer push

---
+++ scripts/00_init.sql
@@ -0,0 +1,17 @@
+-- Create the data warehouse database
+CREATE DATABASE data_warehouse;
+GO
+
+-- Switch to the new database
+USE data_warehouse;
+GO
+
+-- Create Medallion architecture schemas
+CREATE SCHEMA bronze;
+GO
+
+CREATE SCHEMA silver;
+GO
+
+CREATE SCHEMA gold;
+GO
